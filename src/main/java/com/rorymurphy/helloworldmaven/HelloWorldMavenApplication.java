a197b0d9456f8066dff1f92ee9cb207943f1e05d
Rory Murphy <ndmurph04@gmail.com>
2022-03-01 21:07:12 -0500

    Initial commit

---
+++ src/main/java/com/rorymurphy/helloworldmaven/HelloWorldMavenApplication.java
@@ -0,0 +1,13 @@
+package com.rorymurphy.helloworldmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rorymurphy/helloworldmaven/HelloWorldMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rorymurphy.helloworldmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
